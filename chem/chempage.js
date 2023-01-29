8288449fa5c88245d2d8e33d4a4fe9843da98c9c
Noor Nile <aarondunkin123@gmail.com>
2023-01-29 15:29:15 -0500

    Added some notes about chapter 23

--- chem/chempage.js
+++ chem/chempage.js
@@ -4,8 +4,9 @@ const header = document.querySelector('.header');
 const sidebar = document.querySelector('.sidebar');
 const mainDisplay = document.querySelector('.mainDisplay');
 const main = document.querySelector('main');
-const h1 = document.querySelector('#h1');
+const h1 = document.querySelectorAll('#h1');
 const td = document.querySelectorAll('td, th, table');
+const p = document.querySelectorAll('p, li');
 
 
 
@@ -16,8 +17,12 @@ function darkmode() {
         sidebar.style.cssText = "background-color: rgb(20, 15, 36);";
         mainDisplay.style.cssText = "background-color: rgb(20, 15, 36);";
         main.style.cssText = "background-color: rgb(51, 51, 51)";
-        h1.style.color = "rgb(61, 151, 62)";
-        
+        for (let k = 0; k < p.length; k++) {
+            p[k].style.cssText = "color: rgb(61, 151, 62);";
+        }
+        for (let j = 0; j < h1.length; j++) {
+            h1[j].style.cssText = "color: rgb(61, 151, 62);";
+        }
         for (let i = 0; i < td.length; i++) {
             td[i].style.cssText = "color: rgb(61, 151, 62); border: 1px solid rgb(61, 151, 62);";
         }
