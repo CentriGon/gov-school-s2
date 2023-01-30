25d053f8f6e05d7649293ced71e43e8c7500afa1
Noor Nile <aarondunkin123@gmail.com>
2023-01-30 11:08:37 -0500

    Added more chem notes about coordination and solutions. Made sidebar fixed

--- chem/chempage.js
+++ chem/chempage.js
@@ -1,4 +1,7 @@
 const logo = document.querySelector(".mainheader img");
+let coordCompIsDown = false;
+let commonIonIsDown = false;
+let chap12isDown = false;
 
 const header = document.querySelector('.header');
 const sidebar = document.querySelector('.sidebar');
@@ -7,6 +10,48 @@ const main = document.querySelector('main');
 const h1 = document.querySelectorAll('#h1');
 const td = document.querySelectorAll('td, th, table');
 const p = document.querySelectorAll('p, li');
+const cordCompbtn = document.querySelector('.btnCordComp')
+const cordComp = document.querySelector('.notesCoordCompound');
+const commonIonbtn = document.querySelector('.commonIonsbtn');
+const nomenclatureTable = document.querySelector('.nomenclatureTable');
+const chap12Notes = document.querySelector('.chap12Notes');
+const chap12btn = document.querySelector('.chap12btn');
+
+chap12btn.addEventListener('click', () => {
+    if (chap12isDown == false) {
+        chap12Notes.style.cssText = "content-visibility: visible;"
+        chap12isDown = true;
+    }
+    else if (chap12isDown == true) {
+        chap12Notes.style.cssText = "content-visibility: hidden;"
+        chap12isDown = false;
+    }
+})
+
+commonIonbtn.addEventListener('click', () => {
+    if (commonIonIsDown == false) {
+        nomenclatureTable.style.cssText = "content-visibility: visible;"
+        commonIonIsDown = true;
+    }
+    else if (commonIonIsDown == true) {
+        nomenclatureTable.style.cssText = "content-visibility: hidden;"
+        commonIonIsDown = false;
+    }
+})
+
+cordCompbtn.addEventListener('click', () => {
+    if (coordCompIsDown == false) {
+    cordComp.style.cssText = "content-visibility: visible;"
+   
+    coordCompIsDown = true;
+    }
+    else if (coordCompIsDown == true) {
+        cordComp.style.cssText = "content-visibility: hidden;"
+        coordCompIsDown = false;
+        
+    }
+})
+
 
 
 
--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 const logo = document.querySelector(".header img");
 
-const isDark = false;
+let isDark = false;
 logo.addEventListener('click', () => {
     darkmode();
 })
+
 
 
 const header = document.querySelector('.header');
 const sidebar = document.querySelector('.sidebar');
 const mainDisplay = document.querySelector('.mainDisplay');
 const main = document.querySelector('#main');
-style.cssText = ""
+const avisit = document.querySelector('.sidebar li a');
+
 
 
 function darkmode() {
     if (isDark == false) {
         header.style.cssText = "background-color: rgb(20, 15, 36);"
-        sidebar.style.cssText = "background-color: rgb(20, 15, 36);"
+        sidebar.style.cssText = "background-color: rgb(20, 15, 36); color: rgb(61,151,62);"
         mainDisplay.style.cssText = "background-color: rgb(20, 15, 36);"
         main.style.cssText = "background-color: rgb(51, 51, 51)"
-        
+        avisit.style.cssText = "color: rgb(61,151,62);"
         isDark = true;
     }
     
