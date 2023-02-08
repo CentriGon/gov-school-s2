90ea79424f5d92728b3059d9f4f5ee0803bb288b
Noor Nile <aarondunkin123@gmail.com>
2023-02-08 09:52:52 -0500

    Put all drop downs into an array which streamlines process of adding new units into the notes

--- chem/chempage.js
+++ chem/chempage.js
@@ -1,60 +1,12 @@
-const logo = document.querySelector(".mainheader img");
-let coordCompIsDown = false;
-let commonIonIsDown = false;
-let chap12isDown = false;
-
+const logo = document.querySelector(".mainheader img").addEventListener('click', () => {
+    darkmode();
+});
 const header = document.querySelector('.header');
 const sidebar = document.querySelector('.sidebar');
 const mainDisplay = document.querySelector('.mainDisplay');
 const main = document.querySelector('main');
-const h1 = document.querySelectorAll('#h1');
+const h1 = document.querySelectorAll('#h1, p, li');
 const td = document.querySelectorAll('td, th, table');
-const p = document.querySelectorAll('p, li');
-const cordCompbtn = document.querySelector('.btnCordComp')
-const cordComp = document.querySelector('.notesCoordCompound');
-const commonIonbtn = document.querySelector('.commonIonsbtn');
-const nomenclatureTable = document.querySelector('.nomenclatureTable');
-const chap12Notes = document.querySelector('.chap12Notes');
-const chap12btn = document.querySelector('.chap12btn');
-
-chap12btn.addEventListener('click', () => {
-    if (chap12isDown == false) {
-        chap12Notes.style.cssText = "content-visibility: visible;"
-        chap12isDown = true;
-    }
-    else if (chap12isDown == true) {
-        chap12Notes.style.cssText = "content-visibility: hidden;"
-        chap12isDown = false;
-    }
-})
-
-commonIonbtn.addEventListener('click', () => {
-    if (commonIonIsDown == false) {
-        nomenclatureTable.style.cssText = "content-visibility: visible;"
-        commonIonIsDown = true;
-    }
-    else if (commonIonIsDown == true) {
-        nomenclatureTable.style.cssText = "content-visibility: hidden;"
-        commonIonIsDown = false;
-    }
-})
-
-cordCompbtn.addEventListener('click', () => {
-    if (coordCompIsDown == false) {
-    cordComp.style.cssText = "content-visibility: visible;"
-   
-    coordCompIsDown = true;
-    }
-    else if (coordCompIsDown == true) {
-        cordComp.style.cssText = "content-visibility: hidden;"
-        coordCompIsDown = false;
-        
-    }
-})
-
-
-
-
 
 function darkmode() {
     
@@ -62,9 +14,6 @@ function darkmode() {
         sidebar.style.cssText = "background-color: rgb(20, 15, 36);";
         mainDisplay.style.cssText = "background-color: rgb(20, 15, 36);";
         main.style.cssText = "background-color: rgb(51, 51, 51)";
-        for (let k = 0; k < p.length; k++) {
-            p[k].style.cssText = "color: rgb(61, 151, 62);";
-        }
         for (let j = 0; j < h1.length; j++) {
             h1[j].style.cssText = "color: rgb(61, 151, 62);";
         }
@@ -73,6 +22,25 @@ function darkmode() {
         }
 }
 
-logo.addEventListener('click', () => {
-    darkmode();
-})
+const dropDownBtn = document.querySelectorAll('.dropDownBtn');
+const dropDown = document.querySelectorAll('#dropDown');
+
+dropDownArray = [];
+
+for (let i = 0; i < dropDown.length; i++) {
+    dropDownArray[i] = {
+        contents: dropDown[i],
+        button: dropDownBtn[i],
+        isDown: false,
+    }
+    dropDownArray[i].button.addEventListener('click', () => {
+        if (dropDownArray[i].isDown == false) {
+        dropDownArray[i].contents.style.cssText = "content-visibility: visible"
+        dropDownArray[i].isDown = true;
+        }
+        else if (dropDownArray[i].isDown == true) {
+            dropDownArray[i].contents.style.cssText = "content-visibility: hidden"
+            dropDownArray[i].isDown = false;
+            }
+    });
+}
